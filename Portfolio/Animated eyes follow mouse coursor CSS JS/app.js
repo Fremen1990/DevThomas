88c8957cc76d29760334b25dd8f81cfa5fcc2910
Tomasz Stanisz <fremen1990@gmail.com>
2020-05-06 04:53:52 +0200

    New foto and new styles CSS

--- Portfolio/Animated eyes follow mouse coursor CSS JS/app.js
+++ Portfolio/Animated eyes follow mouse coursor CSS JS/app.js
@@ -6,7 +6,7 @@ function eyeMove() {
     let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
     let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
     let radian = Math.atan2(event.pageX - x, event.pageY - y);
-    let rot = radian * (180 / Math.PI) * -1 + 270;
+    let rot = radian * (5000 / Math.PI) * -1 + 180;
     eye.style.transform = `rotate(${rot}deg)`;
   });
 }
